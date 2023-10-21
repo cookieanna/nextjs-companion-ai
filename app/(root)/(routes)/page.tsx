70742d5dedda828454bdc820a494c1b5845c0274
cookieannana <420571732@qq.com>
2023-10-21 15:14:31 +0800

    init database and crate categries tab

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { SearchInput } from "@/components/search-input";
-
-const RootPage = () => {
+import prisma from "@/prisma/client";
+import {Categories} from "@/components/categories";
+const RootPage = async() => {
+    const categories = await prisma.category.findMany()
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data ={categories}/>
         </div>
     );
 }
